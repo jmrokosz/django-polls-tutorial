cb5f6f3805b2bd95f449453042afeb6220bbf420
jRokosz <jacksonrokosz@ssi-mi.com>
2026-01-08 12:25:34 -0500

    Add App Runner config and startup script

--- startup.sh
+++ startup.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-# Set environment variables
-export DJANGO_SETTINGS_MODULE=mysite.settings
-export SECRET_KEY="your-secret-key"
-export DEBUG=False
-export ALLOWED_HOSTS="*"
+# Exit immediately if a command fails
+set -e
 
-# Run Django migrations (optional but recommended)
+# Apply migrations
 python manage.py migrate
+
+# Collect static files
+python manage.py collectstatic --noinput
 
 # Start the Django app with Gunicorn
 gunicorn mysite.wsgi:application --bind 0.0.0.0:8000
