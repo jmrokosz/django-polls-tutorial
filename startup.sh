0de2b230e8f8f5f085bc6617a6ac80a44da5ac32
jRokosz <jacksonrokosz@ssi-mi.com>
2026-01-08 08:52:38 -0500

    App runner config

--- startup.sh
+++ startup.sh
@@ -1,2 +1,5 @@
-#1/bin/bash
-python manage.py collectstatic && gunicorn --workers 2 djangotutorial.wsgi
+#!/bin/sh
+
+python manage.py migrate --noinput
+python manage.py collectstatic --noinput
+gunicorn django_polls_tutorial.wsgi:application --bind 0.0.0.0:8000
