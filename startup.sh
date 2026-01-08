1006af4ada44d68b81393249dd50d1c50d8438f6
jRokosz <jacksonrokosz@ssi-mi.com>
2026-01-08 10:33:50 -0500

    Update App Runner config and startup script

--- startup.sh
+++ startup.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 
-python manage.py migrate --noinput
-python manage.py collectstatic --noinput
-gunicorn django_polls_tutorial.wsgi:application --bind 0.0.0.0:8000
+# Set environment variables
+export DJANGO_SETTINGS_MODULE=mysite.settings
+export SECRET_KEY="your-secret-key"
+export DEBUG=False
+export ALLOWED_HOSTS="*"
+
+# Run Django migrations (optional but recommended)
+python manage.py migrate
+
+# Start the Django app with Gunicorn
+gunicorn mysite.wsgi:application --bind 0.0.0.0:8000
